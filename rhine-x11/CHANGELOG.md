9852d0a18369a6f3c541f4be3fb248447c9909c7
Marc Jakobi <marc.jakobi@tiko.energy>
2024-11-23 16:43:23 +0100

    Add rhine-x11

---
+++ rhine-x11/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for rhine-x11
+
+## 0.1.0.0  -- 2024-11-23
+
+* First version (#ZurichZHF).
---
+++ rhine-x11/src/FRP/Rhine/X11.hs
@@ -0,0 +1,42 @@
+module FRP.Rhine.X11 where
+
+import           Control.Monad             (when)
+import           Data.Functor              (void)
+import           Data.Maybe                (fromMaybe, isJust)
+import           Data.Time.Clock           (getCurrentTime)
+import           FRP.Rhine
+import           Graphics.X11              (allocaSetWindowAttributes,
+                                            cWEventMask, propertyChangeMask,
+                                            set_event_mask)
+import           Graphics.X11.Xlib         (nextEvent, openDisplay)
+import           Graphics.X11.Xlib.Display (defaultRootWindow)
+import           Graphics.X11.Xlib.Event   (allocaXEvent)
+import           Graphics.X11.Xlib.Extras  (Event, changeWindowAttributes,
+                                            getEvent)
+import           Graphics.X11.Xrandr       (xrrQueryExtension,
+                                            xrrUpdateConfiguration)
+import           Prelude
+
+newtype X11Clock = X11Clock{displayName :: Maybe String}
+
+instance MonadIO m => Clock m X11Clock where
+    type Time X11Clock = UTCTime
+    type Tag X11Clock = Event
+    initClock :: X11Clock -> RunningClockInit m (Time X11Clock) (Tag X11Clock)
+    initClock X11Clock{..} = liftIO do
+        display <- openDisplay $ fromMaybe "" displayName
+        let root = defaultRootWindow display
+        allocaSetWindowAttributes $ \setWindowAttrsPtr -> do
+          set_event_mask setWindowAttrsPtr propertyChangeMask
+          changeWindowAttributes display root cWEventMask setWindowAttrsPtr
+        let clock = constM . liftIO . allocaXEvent $ \eventPtr -> do
+              rrData <- xrrQueryExtension display
+              let rrUpdate = when (isJust rrData) . void . xrrUpdateConfiguration
+              nextEvent display eventPtr
+              rrUpdate eventPtr
+              e <- getEvent eventPtr
+              t <- getCurrentTime
+              pure (t, e)
+        (clock,) <$> getCurrentTime
+
+instance GetClockProxy X11Clock
