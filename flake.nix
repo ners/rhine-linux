c72f3394fdd0be3975a70630ab8b3177ecaa0721
ners <ners@gmx.ch>
2025-10-25 15:28:35 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -6,10 +6,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    rhine = {
-      url = "github:turion/rhine";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     ioctl = {
       url = "github:ners/ioctl";
       flake = false;
@@ -25,72 +21,57 @@
         else if isAttrs xs then mapAttrsToList f xs
         else throw "foreach: expected list or attrset but got ${typeOf xs}"
       );
-      hsSrc = root: with lib.fileset; toSource {
+      sourceFilter = root: with lib.fileset; toSource {
         inherit root;
-        fileset = fileFilter (file: any file.hasExt [ "cabal" "hs" "md" ]) ./.;
+        fileset = fileFilter
+          (file: any file.hasExt [ "cabal" "hs" "md" ])
+          root;
       };
-      readDirs = root: attrNames (lib.filterAttrs (_: type: type == "directory") (readDir root));
-      readFiles = root: attrNames (lib.filterAttrs (_: type: type == "regular") (readDir root));
-      basename = path: suffix: with lib; pipe path [
-        (splitString "/")
-        last
-        (removeSuffix suffix)
-      ];
-      cabalProjectPackages = root: with lib; foreach (readDirs root) (dir:
-        let
-          path = "${root}/${dir}";
-          files = readFiles path;
-          cabalFiles = filter (strings.hasSuffix ".cabal") files;
-          pnames = map (path: basename path ".cabal") cabalFiles;
-          pname = if pnames == [ ] then null else head pnames;
-        in
-        optionalAttrs (pname != null) { ${pname} = path; }
-      );
-      cabalProjectPnames = root: lib.attrNames (cabalProjectPackages root);
-      cabalProjectOverlay = root: hfinal: hprev: with lib;
-        mapAttrs
-          (pname: path: hfinal.callCabal2nix pname path { })
-          (cabalProjectPackages root);
-      project = hsSrc ./.;
-      pnames = filter (pname: pname != "kitchen-sink") (cabalProjectPnames project);
       ghcsFor = pkgs: with lib; foldlAttrs
-        (acc: name: hp:
+        (acc: name: hp':
           let
-            version = getVersion hp.ghc;
-            majorMinor = versions.majorMinor version;
-            ghcName = "ghc${replaceStrings ["."] [""] majorMinor}";
+            hp = tryEval hp';
+            version = getVersion hp.value.ghc;
+            ghcName = "ghc${replaceStrings ["."] [""] (versions.majorMinor version)}";
           in
-          if hp ? ghc && ! acc ? ${ghcName} && versionAtLeast version "9.6" && versionOlder version "9.10"
-          then acc // { ${ghcName} = hp; }
+          if hp.success && hp.value ? ghc && ! acc ? ${ghcName} && versionAtLeast version "9.6" && versionOlder version "9.12"
+          then acc // { ${ghcName} = hp.value; }
           else acc
         )
         { }
         pkgs.haskell.packages;
       hpsFor = pkgs: { default = pkgs.haskellPackages; } // ghcsFor pkgs;
+      pnames = map (path: baseNameOf (dirOf path)) (lib.fileset.toList (lib.fileset.fileFilter (file: file.hasExt "cabal") ./.));
+      libPnames = filter (pname: pname != "kitchen-sink") pnames;
+      haskell-overlay = _: prev: hfinal: hprev:
+        with prev.haskell.lib.compose;
+        lib.genAttrs pnames (pname: hfinal.callCabal2nix pname (sourceFilter ./${pname}) { }) // {
+          bindings-libv4l2 = lib.pipe hprev.bindings-libv4l2 [
+            markUnbroken
+            doJailbreak
+            (addPkgconfigDepend prev.libv4l.dev)
+          ];
+          changeset = dontCheck hprev.changeset;
+          i3ipc = doJailbreak (markUnbroken hprev.i3ipc);
+          ioctl = hfinal.callCabal2nix "ioctl" inputs.ioctl { };
+          v4l2 = doJailbreak hprev.v4l2;
+        };
       overlay = lib.composeManyExtensions [
-        inputs.rhine.overlays.default
         (final: prev: {
           haskell = prev.haskell // {
-            packageOverrides = with prev.haskell.lib.compose; lib.composeManyExtensions [
+            packageOverrides = lib.composeManyExtensions [
               prev.haskell.packageOverrides
-              (cabalProjectOverlay project)
-              (hfinal: hprev: {
-                bindings-libv4l2 = lib.pipe hprev.bindings-libv4l2 [
-                  markUnbroken
-                  doJailbreak
-                  (addPkgconfigDepend prev.libv4l.dev)
-                ];
-                i3ipc = doJailbreak (markUnbroken hprev.i3ipc);
-                ioctl = hfinal.callCabal2nix "ioctl" inputs.ioctl { };
-                v4l2 = doJailbreak hprev.v4l2;
-              })
+              (haskell-overlay final prev)
             ];
           };
         })
       ];
     in
     {
-      overlays.default = overlay;
+      overlays = {
+        default = overlay;
+        haskell = haskell-overlay;
+      };
     }
     //
     foreach inputs.nixpkgs.legacyPackages
@@ -98,43 +79,47 @@
         let
           pkgs = pkgs'.extend overlay;
           hps = hpsFor pkgs;
-          bin = pkgs.haskellPackages.callCabal2nix "kitchen-sink" ./kitchen-sink { };
-          name = "rhine-linux";
+          pname = "rhine-linux";
+          bin = hps.default.kitchen-sink;
           libs = pkgs.buildEnv {
-            name = "${name}-libs";
+            name = "${pname}-libs";
             paths = lib.mapCartesianProduct
               ({ hp, pname }: hp.${pname})
-              { hp = attrValues hps; pname = pnames; };
+              { hp = attrValues hps; pname = libPnames; };
             pathsToLink = [ "/lib" ];
           };
           docs = pkgs.buildEnv {
-            name = "${name}-docs";
-            paths = map (pname: pkgs.haskell.lib.documentationTarball hps.default.${pname}) pnames;
+            name = "${pname}-docs";
+            paths = map (pname: pkgs.haskell.lib.documentationTarball hps.default.${pname}) libPnames;
           };
           sdist = pkgs.buildEnv {
-            name = "${name}-sdist";
-            paths = map (pname: pkgs.haskell.lib.sdistTarball hps.default.${pname}) pnames;
+            name = "${pname}-sdist";
+            paths = map (pname: pkgs.haskell.lib.sdistTarball hps.default.${pname}) libPnames;
           };
-          docsAndSdist = pkgs.linkFarm "${name}-docsAndSdist" { inherit docs sdist; };
-          all = pkgs.symlinkJoin {
-            name = "${name}-all";
-            paths = [ bin libs docsAndSdist ];
-          };
+          docsAndSdist = pkgs.linkFarm "${pname}-docsAndSdist" { inherit docs sdist; };
         in
         {
+          ghcs = ghcsFor pkgs;
           formatter.${system} = pkgs.nixpkgs-fmt;
           legacyPackages.${system} = pkgs;
-          packages.${system}.default = all;
-          devShells.${system} = foreach hps (ghcName: hp: {
-            ${ghcName} = hp.shellFor {
-              packages = ps: map (pname: ps.${pname}) pnames;
-              nativeBuildInputs = with hp; [
-                pkgs'.haskellPackages.cabal-install
-                fourmolu
-                haskell-language-server
-              ];
-            };
-          });
+          packages.${system}.default = pkgs.symlinkJoin {
+            name = "${pname}-all";
+            paths = [ bin libs docsAndSdist ];
+            inherit (bin) meta;
+          };
+          devShells.${system} =
+            foreach hps (ghcName: hp: {
+              ${ghcName} = hp.shellFor {
+                packages = ps: map (pname: ps.${pname}) pnames;
+                nativeBuildInputs = (with hps.default; [
+                  cabal-install
+                  cabal-gild
+                  fourmolu
+                ]) ++ lib.optionals (lib.versionAtLeast (lib.getVersion hp.ghc) "9.4") [
+                  hp.haskell-language-server
+                ];
+              };
+            });
         }
       );
 }
